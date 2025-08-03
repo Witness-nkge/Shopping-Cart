01eea9e09892075b226b8aee8d5ad5dacd2b4ef5
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2025-08-03 11:57:43 +0200

    commit

---
+++ shopping_cart.py
@@ -0,0 +1,29 @@
+"""
+shopping cart program that will continuosly ask the user for food product and the price of that product
+have an exit clause if the user wishes to stop adding more things to their car
+at the end show the food items and the total cost to the use
+"""
+
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy or press q to quit: ")
+    if food.lower() == 'q':
+        break
+    else:
+        price = float(input(f"Enter the price of the {food}: R"))
+        foods.append(food)
+        prices.append(price)
+        
+print("------ YOUR CART ------")
+
+for food in foods:
+    print(food) #to show food items horizontally: print(food, end = " ")
+    
+for price in prices:
+    total += price
+    
+print(f"Your total is: R{total}")
